d3bb51316b82084ce2fc2b62f1032d6d3cb6970e
kot-vmeshke <kot-vmeshke.4@gmail.com>
2022-02-06 17:30:26 +0200

    feat: add picture changing function

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
+// adaptive menu
 const openMenu = () => {
   const menuOpen = document.querySelector('.menu-open');
   const menuClose = document.querySelector('.menu-close');
   const headerNav = document.querySelector('.header-nav');
   const navList = document.querySelector('.header-nav__list');
 
-  const open = () => {
-    headerNav.classList.add('adaptive-nav');
-    menuClose.style.display = 'block';
-  }
-
   const close = () => {
     headerNav.classList.remove('adaptive-nav');
     menuClose.style.display = 'none';
+    document.removeEventListener('keydown', (event) => {
+      if (event.key === ('Escape' || 'Esc')) {
+        event.preventDefault();
+        close();
+      }
+    })
+  }
+
+  const open = () => {
+    headerNav.classList.add('adaptive-nav');
+    menuClose.style.display = 'block';
+    document.addEventListener('keydown', (event) => {
+      if (event.key === ('Escape' || 'Esc')) {
+        event.preventDefault();
+        close();
+      }
+    })
   }
 
   menuOpen.addEventListener('click', open);
@@ -27,4 +40,38 @@ const openMenu = () => {
     }
   })
 }
+//change image
+const changeImg = () => {
+  const buttonsWpar = document.querySelector('.buttons-wrap');
+  const buttons = buttonsWpar.querySelectorAll('.btn');
+  const imagesWrap = document.querySelector('.image-wrap');
+  const images = imagesWrap.querySelectorAll('.portfolio-img');
+  const seasons = ['winter', 'spring', 'summer', 'autumn'];
+
+  const preloadImg = () => {
+    seasons.forEach(season => {
+      for(let i = 1; i <= 6; i++) {
+        const img = new Image();
+        img.src = `./assets/img/${season}/${i}.jpg`;
+      }
+    })
+  }
+  preloadImg();
+
+  buttonsWpar.addEventListener('click', (event) => {
+    let season = event.target.dataset.season;
+    if (event.target.classList.contains('btn-outline')) {
+      buttons.forEach(btn => {
+        btn.classList.remove('btn-fill');
+        btn.classList.add('btn-outline');
+      });
+
+      event.target.classList.remove('btn-outline');
+      event.target.classList.add('btn-fill');
+
+      images.forEach((img, index) => img.src = `./assets/img/${season}/${index + 1}.jpg`);
+    }
+  })
+}
+changeImg();
 openMenu();
